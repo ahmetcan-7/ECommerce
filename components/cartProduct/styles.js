c069624fea045ff26ebd22742d4018b67e4ad9bf
Ahmet CAN <ahmetcan423507@gmail.com>
2021-06-01 16:35:43 +0300

    send data to store with addCart function

---
+++ components/cartProduct/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export default makeStyles(() => ({
+  root: {
+    width: '100%'
+  },
+  media: {
+    margin: '0 auto',
+    width: '15rem'
+  },
+  box: {
+    padding: 0,
+    flex: 1
+  },
+  text: {
+    flex: 1
+  }
+}))
