7a10384843cedd41e0524be92af3eccdbafcf506
Ahmet CAN <ahmetcan423507@gmail.com>
2021-06-06 21:36:08 +0300

    calculate total price and display

--- components/selectedProduct/selectedProduct.js
+++ components/selectedProduct/selectedProduct.js
@@ -1,19 +1,26 @@
-import React, { useContext, useEffect } from 'react'
-import { ProductsContext } from '../../contexts/productsContext'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import AddProduct from '../purchase/addProduct'
-function SelectedProduct({ products }) {
-  const { addProduct } = useContext(ProductsContext)
-  console.log('ikinci', addProduct)
 
-  const clickHandle = () => {
-    localStorage.setItem('dataCart', JSON.stringify(products))
+function SelectedProduct({ products }) {
+  let [total, setTotal] = useState(0)
+
+  const totalPrice = () => {
+    if (typeof Storage !== 'undefined') {
+      setTimeout(() => {
+        setTotal(JSON.parse(localStorage.getItem('dataTotal')))
+      }, 1)
+    }
   }
+
+  useEffect(() => {
+    totalPrice()
+  }, [])
 
   return (
     <div>
       <Link href="/">
-        <a onClick={clickHandle}>BackToStore</a>
+        <a>BackToStore</a>
       </Link>
       <div className="products">
         {products.map((product) => (
@@ -24,9 +31,13 @@ function SelectedProduct({ products }) {
             src={product.src}
             price={product.price}
             id={product.id}
+            totalPrice={totalPrice}
+            firstCount={product.count}
           />
         ))}
       </div>
+
+      <h4>{total}</h4>
     </div>
   )
 }
