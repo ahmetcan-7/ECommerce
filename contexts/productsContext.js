456b5f99ecc1cc0ef1c6e60a395f0934082ca0d7
Ahmet CAN <ahmetcan423507@gmail.com>
2021-06-06 21:48:05 +0300

    decreasePrice,increasePrice,totalPrice

--- contexts/productsContext.js
+++ contexts/productsContext.js
@@ -9,42 +9,48 @@ function ProductsContextProvider(props) {
       title: 'MacBook',
       text: 'Uygulamalarda güçlü güncellemeler. Muhteşem yeni tasarım. Son teknoloji ürünü gizlilik ve güvenlik özellikleri. Bugüne kadarki en güçlü yazılımımız, en gelişmiş donanımımızda.',
       price: 19000,
-      src: '/img1.jpg'
+      src: '/img1.jpg',
+      count: 1
     },
     {
       id: 2,
       title: 'iPad',
       text: 'Uygulamalarda güçlü güncellemeler. Muhteşem yeni tasarım. Son teknoloji ürünü gizlilik ve güvenlik özellikleri. Bugüne kadarki en güçlü yazılımımız, en gelişmiş donanımımızda.',
       price: 9000,
-      src: '/img2.webp'
+      src: '/img2.webp',
+      count: 1
     },
     {
       id: 3,
       title: 'iPhone',
       text: 'Uygulamalarda güçlü güncellemeler. Muhteşem yeni tasarım. Son teknoloji ürünü gizlilik ve güvenlik özellikleri. Bugüne kadarki en güçlü yazılımımız, en gelişmiş donanımımızda.',
       price: 13000,
-      src: 'img3.jpg'
+      src: 'img3.jpg',
+      count: 1
     },
     {
       id: 4,
       title: 'Apple Watch',
       text: 'Uygulamalarda güçlü güncellemeler. Muhteşem yeni tasarım. Son teknoloji ürünü gizlilik ve güvenlik özellikleri. Bugüne kadarki en güçlü yazılımımız, en gelişmiş donanımımızda.',
       price: 10000,
-      src: 'img4.jpg'
+      src: 'img4.jpg',
+      count: 1
     },
     {
       id: 5,
       title: 'HomePod',
       text: 'Uygulamalarda güçlü güncellemeler. Muhteşem yeni tasarım. Son teknoloji ürünü gizlilik ve güvenlik özellikleri. Bugüne kadarki en güçlü yazılımımız, en gelişmiş donanımımızda.',
       price: 11000,
-      src: 'img5.png'
+      src: 'img5.png',
+      count: 1
     },
     {
       id: 6,
       title: 'AirPods',
       text: 'Uygulamalarda güçlü güncellemeler. Muhteşem yeni tasarım. Son teknoloji ürünü gizlilik ve güvenlik özellikleri. Bugüne kadarki en güçlü yazılımımız, en gelişmiş donanımımızda.',
       price: 15000,
-      src: 'img6.jpg'
+      src: 'img6.jpg',
+      count: 1
     }
   ]
 
@@ -57,6 +63,7 @@ function ProductsContextProvider(props) {
   }
 
   const [addProduct, setAddProduct] = useState(isItNull)
+  let total = 0
 
   const addCart = (id) => {
     const check = addProduct.every((item) => item.id !== id)
@@ -70,22 +77,52 @@ function ProductsContextProvider(props) {
       alert('The product has been added to cart.')
     }
   }
-  console.log('1.array', addProduct)
+
+  const increasePrice = async (id) => {
+    let data = await JSON.parse(localStorage.getItem('dataCart'))
+    data[id - 1].count += 1
+
+    setAddProduct(data)
+  }
+
+  const decreasePrice = async (id) => {
+    let data = await JSON.parse(localStorage.getItem('dataCart'))
+    data[id - 1].count += -1
+
+    setAddProduct(data)
+  }
 
   useEffect(() => {
     localStorage.setItem('dataCart', JSON.stringify(addProduct))
+
+    addProduct.map((product) => {
+      total = total + product.price * product.count
+    })
+
+    console.log(total)
+
+    localStorage.setItem('dataTotal', JSON.stringify(total))
   }, [addProduct])
 
   useEffect(() => {
     const dataCart = JSON.parse(localStorage.getItem('dataCart'))
     console.log('yeni data', dataCart)
-    if (dataCart) {
+    if (dataCart !== null) {
       setAddProduct(dataCart)
     }
   }, [])
 
   return (
-    <ProductsContext.Provider value={{ products, addProduct, addCart }}>
+    <ProductsContext.Provider
+      value={{
+        products,
+        addProduct,
+        addCart,
+        increasePrice,
+        decreasePrice,
+        total
+      }}
+    >
       {props.children}
     </ProductsContext.Provider>
   )
