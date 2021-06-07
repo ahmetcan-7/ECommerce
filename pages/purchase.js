e814990823ef75b68ab4eea88257f0a84783e995
Ahmet CAN <ahmetcan423507@gmail.com>
2021-06-07 15:58:48 +0300

    add NavbarSecondary component

--- pages/purchase.js
+++ pages/purchase.js
@@ -4,12 +4,15 @@ import SelectedProduct from '../components/selectedProduct/selectedProduct'
 import ProductsContextProvider from '../contexts/productsContext'
 import { sendProducts } from '../components/navbar/navbar'
 import { Container } from '@material-ui/core'
+import NavbarSecondary from '../components/navbarSecondary/navbarSecondary'
+
 function Purchase() {
   const products = sendProducts()
 
   return (
     <div>
       <ProductsContextProvider>
+        <NavbarSecondary />
         <Container>
           <SelectedProduct products={products} />
         </Container>
